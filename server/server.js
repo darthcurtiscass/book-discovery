661e51c62b80da4b1892a786b65415288c978cd2
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-03-30 12:45:21 -0600

    removed || 3001

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
