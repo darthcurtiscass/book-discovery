e442f1275b4cb115575af5cf79911072dc1bcddf
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-03-30 19:31:55 -0600

    rebuild

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -37,7 +37,7 @@ const SignupForm = () => {
       })
 
       console.log(data);
-      Auth.login(data.token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
